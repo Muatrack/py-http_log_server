c9de9654f395cc20f4d02b42f917f1221d94b08b
Muatrack <fjfhccfkuk@163.com>
2025-04-21 16:27:15 +0800

    first commit

---
+++ components/http_server/server.py
@@ -0,0 +1,59 @@
+
+from http.server import HTTPServer
+from http.server import SimpleHTTPRequestHandler
+from http.server import BaseHTTPRequestHandler
+
+routers = [
+    '/v1/api/log'
+]
+
+class HttpSerReqHandle(BaseHTTPRequestHandler):
+    def __init__(self, request, client_address, server):
+        super().__init__(request, client_address, server)
+
+    def _do_get_process_gateway(self):
+        print("[ Got requestpath ]%s" % self.path )
+        print("[ Got preset path ]%s" % routers[0] )
+        if(self.path!=routers[0]):
+            self.send_response(200, 'Got it')
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            self.wfile.write(b"^^^")
+            return
+
+        self.send_response(200, 'Got it')
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        self.wfile.write(b"<<<")
+
+    def _do_post_process_gateway(self):
+        print("[ Got requestpath ]%s" % self.path )
+        print("[ Got preset path ]%s" % routers[0] )
+        if(self.path!=routers[0]):
+            self.send_response(200, 'Got it')
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            self.wfile.write(b"^^^")
+            return
+
+        self.send_response(200, 'Got it')
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        self.wfile.write(b"<<<")
+
+
+    def do_GET(self):
+        self._do_get_process_gateway()
+
+    
+    def do_POST(self):
+        self._do_post_process_gateway()
+
+def start(host:str='127.0.0.1',port:int=8000):
+    bindAddr=(host, port)
+    ser = HTTPServer(bindAddr, HttpSerReqHandle)
+    print('Http server running at %s %d' % (host, port))
+    ser.serve_forever()
+
+if __name__ == "__main__":
+    start()
---
+++ main.py
@@ -0,0 +1,5 @@
+from components.http_server import server
+
+if __name__=='__main__':
+    print("Hi i am Http msg server")
+    server.start()
